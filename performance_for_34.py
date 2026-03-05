fb1ebaf8e8a0f7b980282a7ed915316a1c0c656a
Kaige-Liu <kaigeliu@buaa.edu.cn>
2026-03-05 15:47:48 +0800

    win:现在改一下performance_for_34文件，之前没改

--- performance_for_34.py
+++ performance_for_34.py
@@ -8,10 +8,11 @@ import torch.nn as nn
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
+from models.diffusion_dit import FeatureRestorationDiT
 from models.transceiver import DeepSC, Key_net, Attacker, MAC, CAEM_Fig2_SNR_1D, FeatureMapSelectionModule_SNR_AllC, \
     VerificationDiscriminatorLN, DiffusionSchedule, ConditionalDenoiser
 from utlis.tools import SNR_to_noise, SeqtoText, BleuScore
-from utlis.trainer_for_34 import train_step, val_step, train_mi, greedy_decode, initNetParams
+from utlis.trainer_for_34 import train_step, val_step, greedy_decode, initNetParams, DDIMScheduler
 from dataset.dataloader import return_iter, return_iter_10, return_iter_eve
 from models.transceiver import DeepSC, Key_net, Attacker, MAC, KnowledgeBase, KB_Mapping
 from models.mutual_info import Mine
@@ -28,7 +29,7 @@ parser.add_argument('--d-model', default=128, type=int)
 parser.add_argument('--dff', default=512, type=int)
 parser.add_argument('--num-layers', default=4, type=int)
 parser.add_argument('--num-heads', default=8, type=int)
-parser.add_argument('--batch-size', default=88, type=int)  # 这里控制的是每次拿(从数据集中读取)多少张牌(个句子)
+parser.add_argument('--batch-size', default=512, type=int)  # 这里控制的是每次拿(从数据集中读取)多少张牌(个句子)
 parser.add_argument('--epochs', default=1, type=int)
 
 parser.add_argument('--encoder-num-layer', default=4, type=int, help='The number of encoder layers')
@@ -48,15 +49,14 @@ parser.add_argument('--decoder-dropout', default=0.1, type=float, help='The deco
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-# 需要对解码数据进行BLEU分数计算
-# 输入是解码结果、原文、以及
-def performance(schedule, cdmodel, args, SNR, deepsc, alice_bob_mac, key_ab, eve, Alice_KB, Bob_KB, Eve_KB, Alice_mapping, Bob_mapping, Eve_mapping):
-    # similarity = Similarity(args.bert_config_path, args.bert_checkpoint_path, args.bert_dict_path)
+# 将 cdmodel 和 ddim_scheduler 加入参数列表
+def performance(args, SNR, deepsc, alice_bob_mac, key_ab, Alice_KB, Bob_KB, Alice_mapping, Bob_mapping, cdmodel,
+                ddim_scheduler):
     bleu_score_1gram = BleuScore(1, 0, 0, 0)
+    # bleu_score_2gram = BleuScore(0.5, 0.5, 0, 0)  # 主要关注这几个bleu分数
+    # bleu_score_4gram = BleuScore(0.25, 0.25, 0.25, 0.25)
 
-    test_iterator = return_iter_10(args, 'train')
-    test_iterator_eve = return_iter_eve(args, 'test')
-    iter_eve = iter(test_iterator_eve)
+    test_iterator = return_iter(args, 'test')
 
     StoT = SeqtoText(token_to_idx, end_idx)
     score = []
@@ -64,18 +64,13 @@ def performance(schedule, cdmodel, args, SNR, deepsc, alice_bob_mac, key_ab, eve
     deepsc.eval()
     key_ab.eval()
     alice_bob_mac.eval()
-    eve.eval()
     Alice_KB.eval()
     Bob_KB.eval()
-    Eve_KB.eval()
     Alice_mapping.eval()
     Bob_mapping.eval()
-    Eve_mapping.eval()
-    cdmodel.eval()
-
 
     with torch.no_grad():
-        for epoch in range(1):
+        for epoch in range(1):  # 测试的时候跑三次
             Tx_word = []
             Rx_word = []
 
@@ -86,53 +81,41 @@ def performance(schedule, cdmodel, args, SNR, deepsc, alice_bob_mac, key_ab, eve
 
                 for sents in test_iterator:
                     sents = sents.to(device)
-                    try:
-                        sents_eve = next(iter_eve).to(device)
-                    except:
-                        iter_eve = iter(test_iterator_eve)
-                        sents_eve = next(iter_eve).to(device)
                     # src = batch.src.transpose(0, 1)[:1]
                     target = sents
 
-                    out = greedy_decode(schedule, cdmodel, args, deepsc, alice_bob_mac, key_ab, eve, Alice_KB, Bob_KB, Eve_KB, Alice_mapping, Bob_mapping, Eve_mapping,
-                                                                                        sents, sents_eve,
-                                                                                        noise_std, args.MAX_LENGTH,
-                                                                                        pad_idx,
-                                                                                        start_idx, args.channel)
-
+                    # 把 cdmodel, ddim_scheduler 和 current_snr 传给 greedy_decode
+                    out = greedy_decode(args, deepsc, alice_bob_mac, key_ab, Alice_KB, Bob_KB, Alice_mapping,
+                                        Bob_mapping,
+                                        sents,
+                                        noise_std, args.MAX_LENGTH,
+                                        pad_idx,
+                                        start_idx, args.channel,
+                                        cdmodel=cdmodel, ddim_scheduler=ddim_scheduler, current_snr=snr)
                     # 下面是将数字句子转换为字符串句子
                     sentences = out.cpu().numpy().tolist()  # list bs长度 每个元素是一个句子，句子也是一个List,用数字表示
                     result_string = list(map(StoT.sequence_to_text, sentences))  # list 每个元素是一个字符串句子
                     word = word + result_string  # list 数据集的所有预测句子全加进来
-                    # print(result_string)
 
                     target_sent = target.cpu().numpy().tolist()
                     result_string = list(map(StoT.sequence_to_text, target_sent))
                     target_word = target_word + result_string  # list 数据集的所有原始句子全加进来
-                    # print(result_string, end='\n\n')
 
                 Tx_word.append(word)  # list 长度7 每个元素是list 即Tx_word[0][0]是第一个信噪比下的第一个字符串句子
                 Rx_word.append(target_word)
 
             bleu_score = []
-            sim_score = []
 
             for sent1, sent2 in zip(Tx_word, Rx_word):  # sent1是第一个信噪比下的所有句子
                 # 1-gram
-                bleu_score.append(bleu_score_1gram.compute_score(sent1, sent2))  # 每个元素是list,bleu_score[0][0]是第一个信噪比下的第一个句子的BLEU分数,这样计算了所有的句子
+                bleu_score.append(bleu_score_1gram.compute_score(sent1,
+                                                                 sent2))  # 每个元素是list,bleu_score[0][0]是第一个信噪比下的第一个句子的BLEU分数,这样计算了所有的句子
                 # sim_score.append(similarity.compute_similarity(sent1, sent2)) # 7*num_sent
             bleu_score = np.array(bleu_score)  # 尺寸为7 * 句子数
             bleu_score = np.mean(bleu_score, axis=1)  # 每个信噪比下的所有句子的平均BLEU分数
             score.append(bleu_score)  # 存储到当前epoch中
 
-            # sim_score = np.array(sim_score)
-            # sim_score = np.mean(sim_score, axis=1)
-            # score2.append(sim_score)
-
     score1 = np.mean(np.array(score), axis=0)  # 每个信噪比下的所有句子的平均BLEU分数(按照epoch平均)
-    # score2 = np.mean(np.array(score2), axis=0)
-
-    # return score1, zheng_mac_score, eve_mac_0_score, tamper_0_score, key_0_score  # , score2
     return score1
 
 
@@ -140,7 +123,7 @@ def performance(schedule, cdmodel, args, SNR, deepsc, alice_bob_mac, key_ab, eve
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    SNR = [20]
+    SNR = [-9, -6, -3, 0, 3, 6, 9, 12, 15, 18]
 
     args.vocab_file = args.vocab_file
     vocab = json.load(open(args.vocab_file, 'rb'))
@@ -180,17 +163,18 @@ if __name__ == '__main__':
     Bob_mapping = KB_Mapping().to(device)
     Eve_mapping = KB_Mapping().to(device)
 
-    T = 200
-    schedule = DiffusionSchedule(T=T, device=device)
-    cdmodel = ConditionalDenoiser(
-        feature_dim=128,
-        model_dim=256,
-        num_layers=4,
-        num_heads=8,
+    cdmodel = FeatureRestorationDiT(
+        feature_dim=16,
+        seq_len=32,
+        hidden_size=256,
+        depth=4,
+        num_heads=8
     ).to(device)
 
+    ddim_scheduler = DDIMScheduler(device=device)
+
     checkpoint = torch.load(r'/root/autodl-tmp/restore/checkpoints/checkpoint_109.pth')
-    checkpoint_34 = torch.load(r'/root/autodl-tmp/restore/checkpoints/34/2026-02-02-06_37_46/checkpoint_399_0.8405_0.0004.pth')  # 34部分的那三个网络
+    checkpoint_34 = torch.load(r'/root/autodl-tmp/restore/checkpoints/34/2026-03-05-15_05_48/checkpoint_124_0.9249.pth')  # 34部分的那三个网络
     model_state_dict = checkpoint['deepsc']
     alice_bob_mac_state_dict = checkpoint['alice_bob_mac']
     key_state_dict = checkpoint['key_ab']
@@ -228,6 +212,8 @@ if __name__ == '__main__':
     Eve_mapping = Eve_mapping.to(device)
     cdmodel = cdmodel.to(device)
 
-    bleu_score = performance(schedule, cdmodel, args, SNR, deepsc, alice_bob_mac, key_ab, eve, Alice_KB, Bob_KB, Eve_KB, Alice_mapping, Bob_mapping, Eve_mapping)
+    bleu_score = performance(args, SNR, deepsc, alice_bob_mac, key_ab, Alice_KB, Bob_KB, Alice_mapping, Bob_mapping,
+                             cdmodel, ddim_scheduler)
+
     print(bleu_score)
 
